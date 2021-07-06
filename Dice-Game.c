a18beae8beb1ae95f8cb5dc229f9ccf337519bda
Kashun <49462725+zakio1028@users.noreply.github.com>
2021-07-06 15:54:48 +0900

    Greetings added

--- Dice-Game.c
+++ Dice-Game.c
@@ -5,15 +5,22 @@
 
 int main(void)
 {
-	int i,num=0,sum=0;
-  printf("Rolling the dice...\n");
+	//Greetings part
+	char str[10];
+	printf("What is your name? ");
+	scanf("%s" str);
+	printf("Hello, %s!\n",str);
+	
+	//Rolling the dice part
+  	int i,num=0,sum=0;
+  	printf("Rolling the dice...\n");
 	for (i = 1;i <= 2;i++) {
-    num=rand() % 6 + 1;
+    		num=rand() % 6 + 1;
 		printf("Die %d:%d\n",i,num);
-    sum+=num;
-    num=0;
+    		sum+=num;
+    		num=0;
 	}
-  printf("Total value: %d\n", sum);
+  	printf("Total value: %d\n", sum);
 
 	return 0;
 }
